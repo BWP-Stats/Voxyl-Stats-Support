df233d5d6b39a23c84c8d359f6846fce3454524c
lightninq720 <thelegendrowan@gmail.com>
2022-01-17 20:06:13 +0000

    Added member logs

--- cogs/events.py
+++ cogs/events.py
@@ -23,9 +23,7 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
-        if message.author.id == self.client.user.id:
-            pass
-        else:
+        if not message.author.bot == True:
             channel = self.client.get_channel(logchannel)
             em = discord.Embed(title=f"Message deleted in {message.channel}", description=f"""Message Content: {message.content}""", timestamp=message.created_at)
             em.set_author(name=message.author, icon_url=message.author.avatar_url)
@@ -35,12 +33,28 @@ class Events(commands.Cog):
     async def on_message_edit(self, before, after):
         if not before.author.bot == True:
             channel = self.client.get_channel(logchannel)
-            em = discord.Embed(title=f"Message editted in {before.channel}", description=f"\u200b", timestamp=message.created_at)
+            em = discord.Embed(title=f"Message editted in {before.channel}", description=f"\u200b", timestamp=before.created_at)
             em.add_field(name="Before", value=f"{before[:2000]}")
             em.add_field(name="After", value=f"{after[:2000]}")
-            em.set_author(name=before.author, icon_url=before)
+            em.set_author(name=before.author, icon_url=before.author.avatar_url)
             em.set_footer(text=f"{before.author.id}")
-
+            await channel.send(embed=em)
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.client.get_channel(logchannel)
+        em = discord.Embed(title=f"Member Joined", description=f"""Tag: {member}
+Mention: {member.mention}
+ID: {member.id}""", colour=0x00FF00)
+        em.set_author(name=member, icon_url=member.avatar_url)
+        await channel.send(embed=em)
+    @commands.Cog.listener()
+    async def on_member_leave(self, member):
+        channel = self.client.get_channel(logchannel)
+        em = discord.Embed(title=f"Member Left", description=f"""Tag: {member}
+Mention: {member.mention}
+ID: {member.id}""", colour=0xFF0000)
+        em.set_author(name=member, icon_url=member.avatar_url)
+        await channel.send(embed=em)
 
 
 
