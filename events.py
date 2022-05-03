2161593ac7f2974c589ba6d18b694285a9b3f193
lightninq720 <thelegendrowan@gmail.com>
2022-05-03 19:59:23 +0100

    Added auto-publish

---
+++ events.py
@@ -0,0 +1,17 @@
+import nextcord
+from nextcord.ext import commands, tasks
+from nextcord import Interaction, SlashOption
+from nextcord.abc import GuildChannel
+
+class Events(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id == 965352171040276500 or message.channel.id == 965360224003326033:
+            await message.publish()
+        else:
+            pass
+
