b7b026654ff570beb6fad5a7cdec8a9a629133e7
_lightninq <82113835+lightninq720@users.noreply.github.com>
2022-05-03 20:21:37 +0100

    Update verifymessage.py

--- cogs/verifymessage.py
+++ cogs/verifymessage.py
@@ -13,7 +13,7 @@ class VerifyView(nextcord.ui.View):
     @nextcord.ui.button(label='Verify', style=nextcord.ButtonStyle.blurple, custom_id='verify_view:verify')
     async def create_ticket(self, button: nextcord.ui.Button, ctx: nextcord.Interaction):
         role = ctx.guild.get_role(926955604738707597)
-        if not role in ctx.user.roles():
+        if not role in ctx.user.roles:
             ctx.user.add_roles(926955604738707597)
             await ctx.send("Successfully verified")
         else:
@@ -24,7 +24,7 @@ class Verify(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    @nextcord.slash_command(name="sendverify", description="Send a verification message to a channel", guild_ids=[822791012371005440])
+    @nextcord.slash_command(name="sendverify", description="Send a verification message to a channel", guild_ids=[926955080010301480])
     async def send_ticket(self,
         ctx : Interaction,
         channel : GuildChannel = SlashOption(
@@ -35,7 +35,7 @@ class Verify(commands.Cog):
         )):
         if ctx.user.guild_permissions.administrator == True:
             embed=nextcord.Embed(title="Verify", description=f"Click the button bellow to verify")
-            await channel.send(embed=embed, view=TicketsView())
+            await channel.send(embed=embed, view=VerifyView())
             await ctx.send("Embed sent", ephemeral=True)
         else:
             await ctx.send("You don't have permission to use that", ephemeral=True)
