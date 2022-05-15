285ad43dd348c812ee65e6dccd2e86bb616cd4ea
lightninq720 <thelegendrowan@gmail.com>
2022-05-15 11:10:40 +0100

    Added good filter

--- cogs/events.py
+++ cogs/events.py
@@ -1,7 +1,8 @@
-import nextcord
+import nextcord, json
 from nextcord.ext import commands, tasks
 from nextcord import Interaction, SlashOption
 from nextcord.abc import GuildChannel
+from googleapiclient import discovery
 
 class Events(commands.Cog):
 
@@ -13,7 +14,37 @@ class Events(commands.Cog):
         if message.channel.id == 965352171040276500 or message.channel.id == 965360224003326033:
             await message.publish()
         else:
-            pass
+            if message.author.id == self.client.user.id:
+                return
+            with open("config.json") as f:
+                data = json.load(f)
+                API_KEY = data["GOOGLE_API_KEY"]
+
+            client = discovery.build(
+                "commentanalyzer",
+                "v1alpha1",
+                developerKey=API_KEY,
+                discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+                static_discovery=False,
+            )
+
+            analyze_request = {
+                'comment': { 'text': f'{message.content}' },
+                'requestedAttributes': {'SEVERE_TOXICITY': {}, "THREAT": {}, "IDENTITY_ATTACK": {}, "SEXUALLY_EXPLICIT": {}}
+            }
+
+            response = client.comments().analyze(body=analyze_request).execute()
+            print(json.dumps(response, indent=2))
+            if response['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value'] > 0.9:
+                await message.reply("Would delete with a value of: {} for SEVERE_TOXICITY".format(response['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']))
+            elif response['attributeScores']['THREAT']['summaryScore']['value'] > 0.85:
+                await message.reply("Would delete with a value of: {} for THREAT".format(response['attributeScores']['THREAT']['summaryScore']['value']))
+            elif response['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value'] > 0.85:
+                await message.reply("Would delete with a value of: {} for IDENTITY_ATTACK".format(response['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value']))
+            elif response['attributeScores']['SEXUALLY_EXPLICIT']['summaryScore']['value'] > 0.85:
+                await message.reply("Would delete with a value of: {} for SEXUALLY_EXPLICIT".format(response['attributeScores']['SEXUALLY_EXPLICIT']['summaryScore']['value']))
+            else:
+                message.reply("Would not delete")
 
 
 def setup(client):
