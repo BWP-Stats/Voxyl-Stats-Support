7fa5d58a68f394428f1148783a982826993291d0
lightninq720 <thelegendrowan@gmail.com>
2022-06-20 18:06:20 +0100

    Fixed typo

--- cogs/afk.py
+++ cogs/afk.py
@@ -21,13 +21,13 @@ class Afk(commands.Cog):
             await ctx.user.edit(nick=f"[AFK] " + ctx.user.nick if ctx.user.nick != None else "[AFK] " + ctx.user.name)
             with open("afk.json", "w+") as f:
                 json.dump(data, f)
-            await ctx.send("Your are now **AFK**. Redo this command to cancel this")
+            await ctx.send("You are **now AFK**. Redo this command to cancel this")
         else:
             data["users"].remove(str(ctx.user.id))
             await ctx.user.edit(nick=ctx.user.nick.replace("[AFK] ", "") if ctx.user.nick != None and ctx.user.nick.startswith("[AFK] ") else None)
             with open("afk.json", "w+") as f:
                 json.dump(data, f)
-            await ctx.send("Your are now no longer **AFK**. Redo this command to cancel this")
+            await ctx.send("Your are now **no longer AFK**. Redo this command to cancel this")
 
 
         
